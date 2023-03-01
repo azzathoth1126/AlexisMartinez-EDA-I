29114f43a3afdc4bfde4b9ee3f09e2104aa9e094
Azzathoth1126 <alexis.mta2020@gmail.com>
2023-03-01 15:58:33 +0000

    Segunda parte del codigo

--- Semana_01/practica_1.c
+++ Semana_01/practica_1.c
@@ -1,14 +1,28 @@
 #include<stdio.h>
 
+int tab_torre(){
+
+    return 0;
+}
+
+int tab_alfil(){
+
+    return 0;
+}
+
 int main(){
 
     int tablero[8][8];
     int pieza, alfil = 1, torre = 2;
 
     printf("Escoja que pieza saber sus movimientos: \n");
-    scanf("%d", & pieza)
-    switch (pieza == 1);
-        
+    pirntf("Torre ... 1");
+    printf("Alfil ... 2");
+    scanf("%d", &pieza);
+
+    while pieza == 1{
+        tab_torre();
+    }
 
     return 0;
 }
