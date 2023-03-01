7f85d7f1e5f1c83e40eb721d3c70432ffffabe01
Azzathoth1126 <alexis.mta2020@gmail.com>
2023-03-01 15:53:05 +0000

    Primera parte del codigo

---
+++ Semana_01/practica_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+    int tablero[8][8];
+    int pieza, alfil = 1, torre = 2;
+
+    printf("Escoja que pieza saber sus movimientos: \n");
+    scanf("%d", & pieza)
+    switch (pieza == 1);
+        
+
+    return 0;
+}
