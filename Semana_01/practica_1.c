e3db0ece736e9af1c7b3374c3d3b48e0319c08cd
Azzathoth1126 <alexis.mta2020@gmail.com>
2023-03-01 20:46:08 +0000

    Errores encontrados

--- Semana_01/practica_1.c
+++ Semana_01/practica_1.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 int tab_torre(){
-
+    printf("Escogiste una torre");
     return 0;
 }
 
 int tab_alfil(){
-
+    printf("Escogiste un alfil");
     return 0;
 }
 
@@ -14,12 +14,19 @@ int main(){
 
     int pieza;
 
-    printf("Escoja que pieza saber sus movimientos: \n");
-    printf("Torre ... 1\n");
-    printf("Alfil ... 2\n");
+    printf("Programa que te da los posibles movimiento de una pieza de ajedres.\n");
+    printf("    Torre ....  1\n");
+    printf("    Alfil ....  2\n");
+    printf("Escoja de que pieza saber sus movimientos: ");
+
     scanf("%d", &pieza);
 
-    
+    printf("%d\n", pieza);
 
-    return 0;
+    if (pieza == 1){
+        tab_torre();
+    else 
+        tab_alfil();
+    }
+   return 0;
 }
