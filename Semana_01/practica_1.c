1abaeaae91e1e465128632ff931ba070df666575
Azzathoth1126 <alexis.mta2020@gmail.com>
2023-03-03 16:30:39 +0000

    me canse

--- Semana_01/practica_1.c
+++ Semana_01/practica_1.c
@@ -31,6 +31,41 @@ void fn_tab_ajedrez(){
     }
 }
 
+int fn_tab_ajedrez_cor(k,l){
+    int i,j,k,l;
+    int tab_a[8][8];//variable tablero de ajedrez
+
+    //Imprime las coordenadas de las filas
+    printf("Filas   "); 
+    for(i=0; i<8; i++)
+        printf("%i ", (cont+=1));
+    printf("  Columnas\n\n");
+
+    //Rellena el arreglo bidimensional con 0
+    for (i=0; i<8; tab_a[i++][j]){
+        if (i = k)
+            tab_a[i][j] = 1;
+        tab_a[i][j] = 0;
+        for (j=0; j<9; tab_a[i][j++]){
+            if (j = l)
+                tab_a[i][j] = 1;
+            tab_a[i][j] = 0;
+        }
+    }
+
+    //Imprime el arreglo
+    for (i=0; i<8; tab_a[i++][j]){
+        printf("\t");
+        printf("%i ", tab_a[i][j]);
+        for (j=0; j<7; tab_a[i][j++]){
+            printf("%i ", tab_a[i][j]);
+        }
+        printf("  %i", (cont2+=1)); //Coordenadas de las columnas
+        printf("\n");
+    }
+
+}
+
 int tab_torre(){
 
     int fila, colm, i, j; //fila y columna
@@ -40,6 +75,7 @@ int tab_torre(){
     fn_tab_ajedrez();
     printf("\nRecuerda que el tablero de ajedrez se divide en filas y columnas del 1 a la 8\n");
 
+    //Pide las coordenadas para la pieza
     printf("\nEn que fila de la 1 a la 8 quieres posicionar la torre?: ");
     scanf("%i", &fila);
     while(fila<1 || fila>8){
@@ -47,6 +83,7 @@ int tab_torre(){
         scanf("%d", &fila);
     }
 
+    //Pide las coordenadas para la pieza
     printf("\nEn que columna de la 1 a la 8 quieres posicionar la torre?: ");
     scanf("%i", &colm);
     while(colm<1 || colm>8){
@@ -54,6 +91,7 @@ int tab_torre(){
         scanf("%d", &colm);
     }
 
+    fn_tab_ajedrez_cor(fila,colm);
     printf("\n\n");
 
     return 0;
