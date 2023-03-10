767eb80e5d9566f213928cf1e26f02593eac05ea
Azzathoth1126 <alexis.mta2020@gmail.com>
2023-03-10 06:03:09 +0000

    funcion tablero vacio listo

--- Semana_02/practica_02.c
+++ Semana_02/practica_02.c
@@ -2,30 +2,29 @@
 
 void tablero_vacio(){
 
-    int i, j, cont=0, cont2=0; //fila y columna
+    int i, j, c1=0, c2=0; //fila y columna
     int tab_ajedrez[8][8];
 
     /*Rellena el arreglo bidimensional con 0*/
     for (i=0; i<8; tab_ajedrez[i++][j]){
-        tab_ajedrez[i][j] = 0;
-        for (j=0; j<9; tab_ajedrez[i][j++]){
+        for (j=0; j<9; tab_ajedrez[i][j++])
             tab_ajedrez[i][j] = 0;
-        }
     }
 
     //Imprime las coordenadas de las filas 
     printf("Filas   ");
     for(i=0; i<8; i++)
-        printf("%i ", (cont+=1));
+        printf("%i ", (c1+=1));
     printf("  Columnas\n\n");
 
     //Imprime el tablero
     for (i=0; i<8; tab_ajedrez[i++][j]){
-        printf("\t%i ", tab_ajedrez[i][j]);
-        for (j=0; j<7; tab_ajedrez[i][j++]){
+        printf("\t");
+        for (j=0; j<8; tab_ajedrez[i][j++])
             printf("%i ", tab_ajedrez[i][j]);
-        }
-        printf("  %i", (cont2+=1)); //Coordenadas de las columnas
+
+        //Imprime las coordenadas de las columnas
+        printf("  %i", (c2+=1));
         printf("\n");
     }
 }
