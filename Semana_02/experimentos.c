4041bdeb693541b66d641fe241260a65cec1b7cb
Azzathoth1126 <alexis.mta2020@gmail.com>
2023-03-10 23:11:38 +0000

    experimento listo

--- Semana_02/experimentos.c
+++ Semana_02/experimentos.c
@@ -1,39 +1,20 @@
 #include <stdio.h>
 
-int main(){
-    /*int *ap, indice;
-    int nums[3][3] = {{99,88,77}, {66,55,44}, {33,22,11}};
-    ap = nums;
+int main() {
+    
+    int arreglo1[8][8];
+    int *(*apt);
+    apt = arreglo1;
 
-    for(indice = 0; indice < 9; indice++){
-        if((indice%3)==0){
-            printf("\n");
-        }
-        printf("%x\t", (ap+indice));
+    for(int a=0; a<8; a++){
+        for(int b=0; b<8; b++)
+            arreglo1[a][b] = 0;
     }
 
-    printf("\n\n");*/
-
-    int i, j;//fila y columna
-    int tab_ajedrez[8][8];
-    int *ap;
-    ap = &tab_ajedrez;
-
-    /*Rellena el arreglo bidimensional con 0*/
-    for (i=0; i<8; i++){
-        *(*ap) = 0;
-        for (j=0; j<9; j++){
-            *(*ap)=0
-            *(*ap++);
-        }
-        *(*(ap++));
-    }
-
-    for(i=0; i<8; i++){
-        printf("%d", tab_ajedrez[i][j]);
-        for(j=0; j<8; j++){
-           printf("%d", tab_ajedrez[i][j]); 
-        }
+    for(int i=0; i<8; *(*(apt+(i++)))){
+        for(int j=0; j<8; *(*apt+(j++)))
+            printf("%d ", *apt);
+        printf("\n");
     }
 
     return 0;
