758b25869dcedb1ccaad68c598af31a6c5d0e6a8
Azzathoth1126 <alexis.mta2020@gmail.com>
2023-03-15 17:50:39 +0000

    agregar punteros y movimientos de las piezas

---
+++ Semana_03/practica_3.c
@@ -0,0 +1,136 @@
+#include<stdio.h>
+
+struct { //Estructura que contiene las coordenadas para las piezas
+    int cfi;
+    int ccol;
+}torre, caballo, alfil, rey, reina;
+
+//Arreglar para imprimir con apuntadores
+void fTabVacio(){
+    int i, j, c1=0, c2=0; //fila y columna
+    int tAjedrez[8][8];
+    int *(*ap);
+    ap = &tAjedrez;
+
+    /*Rellena el arreglo bidimensional con 0*/
+    for (i=0; i<8; tAjedrez[i++][j]){
+        for (j=0; j<9; tAjedrez[i][j++])
+            tAjedrez[i][j] = 0;
+    }
+
+    //Imprime las coordenadas de las filas 
+    printf("Filas   ");
+    for(i=0; i<8; i++)
+        printf("%i ", (c1+=1));
+    printf("  Columnas\n\n");
+
+    //Imprime el tablero
+    for (i=0; i<8; tAjedrez[i++][j]){
+        printf("\t");
+        for (j=0; j<8; tAjedrez[i][j++])
+            printf("%i ", tAjedrez[i][j]);
+
+        //Imprime las coordenadas de las columnas
+        printf("  %i\n", (c2+=1));
+    }
+}
+
+
+void fTorre();
+void fAlfil();
+void fCaballo();
+void fReina();
+void fRey();
+
+
+int main(){ //Programa principal
+
+    int pieza;
+
+    printf("\nPosibles movimiento de una pieza de ajedrez.\n\n");
+    fTabVacio();
+    printf("\nPiezas disponibles\n\n");
+    printf("    Torre   ....  1\n");
+    printf("    Caballo ....  2\n");
+    printf("    Alfil   ....  3\n");
+    printf("    Reina   ....  4\n");
+    printf("    Rey     ....  5\n");
+    printf("    Salir   ....  \n\n");
+
+    printf("De que pieza quieres saber sus posibles movimientos: ");
+    scanf("%d", &pieza);
+
+    switch(pieza){
+        case 1:
+        fTorre();
+        break;
+
+        case 2:
+        fCaballo();
+        break;
+
+        case 3:
+        fAlfil();
+        break;
+
+        case 4:
+        fReina();
+        break;
+
+        case 5:
+        fRey();
+        break;
+
+        default:
+        printf("Escogiste salir.\n");
+        printf("Programa finalizado\n\n");
+        break;
+    }
+    return 0;
+}
+
+
+void fTorre(){
+
+    printf("Escogiste la pieza \"Torre\" \n\n");
+    printf("Dime en que parte del tablero quieres colocar la pieza\n");
+    printf("Fila: "); scanf("%d", &torre.cfi);
+    printf("Columna: "); scanf("%d", &torre.ccol);
+
+};
+
+
+void fAlfil(){
+
+    printf("Escogiste la pieza \"Alfil\" \n\n");
+    printf("Dime en que parte del tablero quieres colocar la pieza\n");
+    printf("Fila: "); scanf("%d", &alfil.cfi);
+    printf("Columna: "); scanf("%d", &alfil.ccol);
+};
+
+
+void fCaballo(){
+
+    printf("Escogiste la pieza \"Caballo\" \n\n");
+    printf("Dime en que parte del tablero quieres colocar la pieza\n");
+    printf("Fila: "); scanf("%d", &caballo.cfi);
+    printf("Columna: "); scanf("%d", &caballo.ccol);
+};
+
+
+void fReina(){
+
+    printf("Escogiste la pieza \"Reina\" \n\n");
+    printf("Dime en que parte del tablero quieres colocar la pieza\n");
+    printf("Fila: "); scanf("%d", &reina.cfi);
+    printf("Columna: "); scanf("%d", &reina.ccol);
+};
+
+
+void fRey(){
+
+    printf("Escogiste la pieza \"Rey\" \n\n");
+    printf("Dime en que parte del tablero quieres colocar la pieza\n");
+    printf("Fila: "); scanf("%d", &rey.cfi);
+    printf("Columna: "); scanf("%d", &rey.ccol);
+};
