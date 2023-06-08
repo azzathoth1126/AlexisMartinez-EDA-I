332c073c0663c48e9c4ac92017f142117b12bef3
Azzathoth1126 <alexis.mta2020@gmail.com>
2023-06-08 16:42:48 +0000

    codigo lista

--- Semana_10/practica_10.py
+++ Semana_10/practica_10.py
@@ -1,58 +1,65 @@
 def add_book(biblioteca):
-
-    titulo = str(input("\nTitulo: "))
+    titulo = input("\nTitulo: ")
     idn = int(input("IDN: "))
 
-    elemento = {'Titulo':titulo, 'IDN':idn}
+    elemento = {'Titulo': titulo, 'IDN': idn}
 
     biblioteca.append(elemento)
 
 
-def delet_book(biblioteca):
+def delete_book(biblioteca):
     print("\n   Libro - IDN")
     n = 1
-    for libro in biblioteca:    
+    for libro in biblioteca:
         print("{}. {}".format(n, libro))
-        n = n+1
-    
-    x = int(input("Que elemeto quiere eliminar: "))
-    biblioteca.pop((x-1))
+        n = n + 1
+
+    x = int(input("Qué elemento quieres eliminar: "))
+    biblioteca.pop((x - 1))
 
 
 def order_books(biblioteca):
-    lista_odenada = sorted(biblioteca, key=lambda x: x['IDN'])
-    return lista_odenada
+    for i in range(len(biblioteca)):
+        for j in range(len(biblioteca) - i - 1):
+            valor_actual = biblioteca[j].get('Titulo')
+            valor_siguiente = biblioteca[j+1].get('Titulo')
+
+            if valor_actual is not None and valor_siguiente is not None and valor_actual > valor_siguiente:
+                biblioteca[j], biblioteca[j+1] = biblioteca[j+1], biblioteca[j]
+
+    return biblioteca
 
 
-def books(biblioteca):
+def show_books(biblioteca):
     n = 1
-    for libro in biblioteca:    
+    for libro in biblioteca:
         print("{}. {}".format(n, libro))
-        n = n+1
+        n = n + 1
 
 
 if __name__ == '__main__':
-
     x = 1
     biblioteca = []
 
     while x != 5:
         x = int(input('''
-        \nBienvenido a la biblioteca, que movimiento quieres realizar:
+        \n\nBienvenido a la biblioteca, ¿qué movimiento quieres realizar?
 
         1. Agregar libro
         2. Borrar libro
         3. Ordenar biblioteca
         4. Ver biblioteca
-        5. Salir \n\n'''))
+         5. Salir \n\n'''))
 
         if x == 1:
             add_book(biblioteca)
         elif x == 2:
-            delet_book(biblioteca)
+            delete_book(biblioteca)
         elif x == 3:
-            order_books(biblioteca)
+            biblioteca = order_books(biblioteca)
+            show_books(biblioteca)
         elif x == 4:
-            books(biblioteca)
+            show_books(biblioteca)
         else:
             x = 5
+
